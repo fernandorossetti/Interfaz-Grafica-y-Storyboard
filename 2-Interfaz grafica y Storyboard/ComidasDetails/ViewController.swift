7f856e78cdfde2497be782cd4d23ab28d549c881
fernando rossetti <fernando_r_4@hotmail.com>
2018-11-17 17:06:56 -0300

    Add files via upload

---
+++ 2-Interfaz grafica y Storyboard/ComidasDetails/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ComidasDetails
+//
+//  Created by Mateo Olaya Bernal on 11/26/15.
+//  Copyright © 2015 NextU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
